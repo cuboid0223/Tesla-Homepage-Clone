a9d9de788d935c0bc323ce19226999614c7c2356
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2021-02-22 21:55:01 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-
+import Header from "./components/Header/Header";
 function App() {
-  return <div className="app">
-    sss
-  </div>;
+  return (
+    <div className="app">
+      {/* {header} */}
+      <Header />
+      {/* item => snapping */}
+      {/* item => snapping */}
+      {/* item => snapping */}
+      {/* item => snapping */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import TeslaLogo from "../../assets/teslaLogoSmall.svg";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={TeslaLogo} alt="TeslaLogo" />
+      </div>
+
+      <ul className="header__center">
+        <li>
+          <a href="#"> Model S</a>
+        </li>
+        <li>
+          <a href="#">Model 3</a>
+        </li>
+        <li>
+          <a href="#">Model X</a>
+        </li>
+        <li>
+          <a href="#">Model Y</a>
+        </li>
+        <li>
+          <a href="#">CYBERTRUCK</a>
+        </li>
+        <li>
+          <a href="#">POWERWALL</a>
+        </li>
+      </ul>
+
+      <div className="header__right">
+        <a href="#">TESLA 帳戶</a>
+        {/* hamburger icon */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
